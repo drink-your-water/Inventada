77f5a32581e3eb661057f474a67f02fe94fc8ab3
drink-your-water <mainornn00@gmail.com>
2024-10-06 12:53:12 +0800

    Replaced Frontend with Server and Webassembly Setup

--- Frontend/InvenTadaFrontend/InvenTadaFrontend/Program.cs
+++ Frontend/InvenTadaFrontend/InvenTadaFrontend/Program.cs
@@ -4,12 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -22,6 +27,8 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(InvenTadaFrontend.Client._Imports).Assembly);
 
 app.Run();
